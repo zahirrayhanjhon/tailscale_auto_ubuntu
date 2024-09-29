ed1bb8e0258913e2ba154ac3b2e1500bd6b28dcf
Md. Zahir Rayhan <147940712+zahirrayhanjhon@users.noreply.github.com>
2024-09-30 05:34:05 +0600

    install.sh

--- install.sh
+++ install.sh
@@ -2,31 +2,39 @@
 
 # Update the package list
 apt update
-sleep 5  # Wait for 5 seconds
+sleep 1  # Wait for 1 seconds
 
 # Upgrade the installed packages
 apt upgrade -y
-sleep 5  # Wait for 5 seconds
+sleep 1  # Wait for 1 seconds
 
 # Install sudo and curl
 apt install -y sudo curl
-sleep 5  # Wait for 5 seconds
+sleep 1  # Wait for 1 seconds
 
 # Enable IPv4 forwarding
 sudo sysctl -w net.ipv4.ip_forward=1
-sleep 5  # Wait for 5 seconds
+sleep 1  # Wait for 1 seconds
 
 # Enable IPv6 forwarding
 sudo sysctl -w net.ipv6.conf.all.forwarding=1
-sleep 5  # Wait for 5 seconds
+sleep 1  # Wait for 1 seconds
 
 # Install Tailscale
 curl -fsSL https://tailscale.com/install.sh | sh
-sleep 5  # Wait for 5 seconds
+sleep 1  # Wait for 1 seconds
 
 # Start Tailscale daemon
 sudo tailscaled --state=/var/lib/tailscale/tailscaled.state &
-sleep 5  # Wait for 5 seconds
+sleep 1  # Wait for 1 seconds
+
+# Install Netbird
+curl -fsSL https://pkgs.netbird.io/install.sh | sh
+sleep 1  # Wait for 1 seconds 
 
 # Bring up Tailscale
 sudo tailscale up
+
+# Bring up Netbird
+sudo netbird up
+sleep 15  # Wait for 15 seconds to copy Netbird login command
